c7a14a8a145aec8d060eea062cf4ca3cf6956c47
SunLn <dengchenhua@gmail.com>
2014-09-26 19:07:34 +0800

    import baidu webuploader

---
+++ routes/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'ACCESS_KEY': '',
+    'SECRET_KEY': '',
+    'Bucket_Name': '',
+    'Port': 18080,
+    'Uptoken_Url': '/uptoken',
+    'Domain': ''
+};
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,32 @@
 var express = require('express');
+var config = require('./config');
+var qiniu = require('qiniu');
+
 var router = express.Router();
+
+qiniu.conf.ACCESS_KEY = config.ACCESS_KEY;
+qiniu.conf.SECRET_KEY = config.SECRET_KEY;
 
 /* GET home page. */
 router.get('/', function(req, res) {
     res.render('index', {
-        title: 'Express'
+        domain: config.domain,
+        uptoken_url: config.Uptoken_Url,
+        title: 'Metro UI Blog'
     });
+});
+
+router.get('/uptoken', function(req, res, next) {
+    var uptoken = new qiniu.rs.PutPolicy(config.Bucket_Name);
+    var token = uptoken.token();
+    res.header("Cache-Control", "max-age=0, private, must-revalidate");
+    res.header("Pragma", "no-cache");
+    res.header("Expires", 0);
+    if (token) {
+        res.json({
+            uptoken: token
+        });
+    }
 });
 
 module.exports = router;
