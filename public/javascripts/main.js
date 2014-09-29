36d7b4d3f889aa705491de0c9b7686273743c6a5
SunLn <dengchenhua@gmail.com>
2014-09-29 13:22:26 +0800

    update,replace webuploader with plupload

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,90 +1,87 @@
-(function($) {
-    // 当domReady的时候开始初始化
-    $(function() {
-        var $upload = $('#uploadBtn');
-
-        var uploader = WebUploader.create({
-            pick: {
-                id: '#filePicker'
-            },
-            paste: '#uploader',
-            chunked: false,
-            chunkSize: 512 * 1024,
-            server: 'http://up.qiniu.com',
-
-            // accept: {
-            //     title: 'Images',
-            //     extensions: 'gif,jpg,jpeg,bmp,png',
-            //     mimeTypes: 'image/*'
-            // },
-
-            // 禁掉全局的拖拽功能。这样不会出现图片拖进页面的时候，把图片打开。
-            disableGlobalDnd: true,
-            fileNumLimit: 300,
-            fileSizeLimit: 200 * 1024 * 1024, // 200 M
-            fileSingleSizeLimit: 50 * 1024 * 1024 // 50 M
-        });
-
-        $.get('/uptoken').success(function(data) {
-            $('#token').val(data.uptoken);
-            uploader.options.formData.token = data.uptoken
-        });
-
-        uploader.on('ready', function() {
-            window.uploader = uploader;
-        });
-
-        uploader.on('uploadBeforeSend', function(file, data) {
-            data.key = data.name;
-        });
-
-        uploader.on('uploadProgress', function(file, percentage) {
-
-        });
-
-        uploader.on('fileDequeued', function(file) {
-
-        });
-
-        uploader.on('uploadSuccess', function(file, data) {
-            // $('#' + file.id).find('p.state').text('已上传');
-            console.log(file, data);
-            console.log(data.key)
-        });
-
-        uploader.on('uploadError', function(file) {
-            // $('#' + file.id).find('p.state').text('上传出错');
-        });
-
-        uploader.on('all', function(type) {
-
-        });
-
-        uploader.onError = function(code) {
-            alert('Eroor: ' + code);
-        };
-
-        $upload.on('click', function() {
-            uploader.upload();
-        });
-
-    });
-
-})(jQuery);
-
 $(function() {
+
     $('.icon-share-3').on('click', function() {
-        $.Dialog({
-            overlay: true,
-            shadow: true,
-            flat: true,
-            icon: '<img src="images/excel2013icon.png">',
-            title: 'Markdown To HTML',
-            content: '',
-            onShow: function(_dialog) {
-                var content = _dialog.children('.content');
-                content.html('Set content from event onShow');
+        $('#js-md-2-html').removeClass('hide');
+
+        var Qiniu = new QiniuJsSDK();
+
+        var uploader = Qiniu.uploader({
+            runtimes: 'html5,flash', //上传模式,依次退化
+            browse_button: 'upMD', //上传选择的点选按钮，**必需**
+            uptoken_url: '/uptoken', //Ajax请求upToken的Url，**强烈建议设置**（服务端提供）
+            unique_names: true, // 默认 false，key为文件名。若开启该选项，SDK会为每个文件自动生成key（文件名）
+            domain: 'http://metro-demo-qiniu.qiniudn.com', //bucket 域名，下载资源时用到，**必需**
+            container: 'upMDBox', //上传区域DOM ID，默认是browser_button的父元素，
+            max_file_size: '100mb', //最大文件体积限制
+            flash_swf_url: './js-sdk/Moxie.swf', //引入flash,相对路径
+            max_retries: 3, //上传失败最大重试次数
+            dragdrop: true, //开启可拖曳上传
+            drop_element: 'upMDBox', //拖曳上传区域元素的ID，拖曳文件或文件夹后可触发上传
+            chunk_size: '4mb',
+            auto_start: true,
+            filters: {
+                mime_types: [{
+                    title: "Markdown files",
+                    extensions: "md,MD,markdown"
+                }]
+            },
+            init: {
+                'FilesAdded': function(up, files) {
+                    plupload.each(files, function(file) {
+                        // 文件添加进队列后,处理相关的事情
+                    });
+                },
+                'BeforeUpload': function(up, file) {
+                    // 每个文件上传前,处理相关的事情
+                },
+                'UploadProgress': function(up, file) {
+                    // 每个文件上传时,处理相关的事情
+                },
+                'FileUploaded': function(up, file, info) {
+                    var domain = up.getOption('domain');
+                    var res = $.parseJSON(info);
+                    var sourceLink = domain + res.key;
+                    // 获取上传成功后的文件的Url
+                },
+                'Error': function(up, err, errTip) {
+                    //上传出错时,处理相关的事情
+                },
+                'UploadComplete': function() {
+                    //队列文件处理完毕后,处理相关的事情
+                }
             }
         });
+
+        // var tempHtml = $('#js-md-2-html').find('.container');
+        // $.Dialog({
+        //     overlay: true,
+        //     shadow: true,
+        //     flat: true,
+        //     icon: '<img src="images/excel2013icon.png">',
+        //     title: 'Markdown To HTML',
+        //     // content: '<div class="span8 offset1 container"><label class="inline-block">Upload Markdown :</label><div id="upMDBox" data-role="input-control" class="input-control text"><div><input type="input" tabindex="-1" style="z-index: 0;"><button id="upMD" type="button" class="btn-file"></button></div></div><div id="upMDBtn" class="button small info"> 开始上传</div><label>Choose Mode :</label><div data-role="input-control" class="input-control radio"><label class="inline-block"><input type="radio" name="md-mode" checked="" value="0"><span class="check"></span>Render All</label><label class="inline-block offset1"><input type="radio" name="md-mode" value="1"><span class="check"></span>Only Render Body</label></div><label>CSS URL :</label><div data-role="input-control" class="input-control text"><input type="text" placeholder=""><button tabindex="-1" type="button" class="btn-clear"></button></div><input type="submit" value="Submit"></div>',
+        //     onShow: function(_dialog) {
+        //         var content = _dialog.children('.content');
+        //         // if (content.find('.container').length == 0) {
+        //         //     // temp.appendTo(content).;
+        //         content.append($('#js-md-2-html').find('.container') || tempHtml);
+        //         content.addClass('span10').parent('.window').css('height', '');
+        //         // }
+        //         // content.append()
+        //         $.Metro.initInputs();
+        //         console.log($('#upMD'));
+
+        //     }
+        // });
+
+
+
+        $('#upMDBtn').on('click', function() {
+            uploader.start();
+        });
+        // $('#upMD .btn-file').on('click', function() {
+        //     $('#upMD').trigger('click');
+        // });
+
     });
 });
