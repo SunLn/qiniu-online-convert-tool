8d5854a74da3cb5804ecec6657176fb0c03f0b1b
SunLn <dengchenhua@gmail.com>
2014-10-10 23:03:37 +0800

    config.js

--- routes/config.js
+++ routes/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    'ACCESS_KEY': '0MLvWPnyya1WtPnXFy9KLyGHyFPNdZceomLVk0c9',
-    'SECRET_KEY': 'o5itRgrXxoD6XQ5wDWKQ7h--eWvWyQVKcsIURuEV',
+    'ACCESS_KEY': '',
+    'SECRET_KEY': '',
     'Bucket_Name': 'metro-demo-qiniu',
     'Port': 18080,
     'Uptoken_Url': '/uptoken',
