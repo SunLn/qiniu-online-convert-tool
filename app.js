d9bc1d437632baa13d46663bb9d38e7cf4d4b697
SunLn <dengchenhua@gmail.com>
2014-10-10 22:51:11 +0800

    fix app.js

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var debug = require('debug')('qiniu-online-tool');
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
