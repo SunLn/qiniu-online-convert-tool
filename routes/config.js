0f44cc848a1110a52b8dad0e39299502a38b58b0
SunLn <dengchenhua@gmail.com>
2014-10-10 22:49:31 +0800

    add config.js

---
+++ routes/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'ACCESS_KEY': '0MLvWPnyya1WtPnXFy9KLyGHyFPNdZceomLVk0c9',
+    'SECRET_KEY': 'o5itRgrXxoD6XQ5wDWKQ7h--eWvWyQVKcsIURuEV',
+    'Bucket_Name': 'metro-demo-qiniu',
+    'Port': 18080,
+    'Uptoken_Url': '/uptoken',
+    'Domain': 'http://metro-demo-qiniu.qiniudn.com'
+};
